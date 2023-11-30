0f95f2622ffde5a33d40609e213944c92eb8345d
Lieven <lieven@risolto.be>
2023-11-30 11:59:09 +0100

    day1 improved

--- day1.erl
+++ day1.erl
@@ -7,12 +7,9 @@ process_input(Path) ->
     binary_to_list(File).
 
 count_brackets([], Open, Close, _Level, CellarEntry) -> {{part1, Open-Close}, {part2, CellarEntry}};
-count_brackets([H|T], Open, Close, _Level, CellarEntry) when [H]=="(" -> count_brackets(T, Open+1, Close, Open+1-Close, CellarEntry);
-count_brackets([H|T], Open, Close, 0, 0) when [H]==")" -> count_brackets(T, Open, Close+1, Open-Close-1, Open+Close+1);
-count_brackets([H|T], Open, Close, _Level, CellarEntry) when [H]==")" -> count_brackets(T, Open, Close+1, Open-Close-1, CellarEntry).
-%count_brackets([40|T], Open, Close, _Level, CellarEntry) -> count_brackets(T, Open+1, Close, Open+1-Close, CellarEntry);
-%count_brackets([41|T], Open, Close, 0, 0) -> count_brackets(T, Open, Close+1, Open-Close-1, Open+Close+1);
-%count_brackets([41|T], Open, Close, _Level, CellarEntry) -> count_brackets(T, Open, Close+1, Open-Close-1, CellarEntry).
+count_brackets([40|T], Open, Close, _Level, CellarEntry) -> count_brackets(T, Open+1, Close, Open+1-Close, CellarEntry);
+count_brackets([41|T], Open, Close, 0, 0) -> count_brackets(T, Open, Close+1, Open-Close-1, Open+Close+1);
+count_brackets([41|T], Open, Close, _Level, CellarEntry) -> count_brackets(T, Open, Close+1, Open-Close-1, CellarEntry).
 
 solve() ->
     Input = process_input("./input/day1.txt"),
