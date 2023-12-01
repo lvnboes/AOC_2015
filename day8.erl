a0d1774dcb5a1fc0b7387eb6d47106c25200f868
lvnboes <lvn.boes@gmail.com>
2023-12-01 02:14:01 +0100

    day8_even_shorter:-)

--- day8.erl
+++ day8.erl
@@ -8,21 +8,11 @@ process_input(Path) ->
     {ok, File} = file:read_file(Path),
     flatten([binary_to_list(X) || X <- split(File, <<"\n">>, [global])]).
 
-is_hex_char(Char) -> is_hex_char(Char, "0123456789abcdef").
-is_hex_char(_Char, []) -> false;
-is_hex_char(Char, [H|_T]) when Char == H -> true;
-is_hex_char(Char, [_H|T]) -> is_hex_char(Char, T).
-
 count_chars(Chars) -> count_chars(Chars, 0, 0, 0).
 count_chars([], Acc1, Acc2, Acc3) -> {{part1, Acc1-Acc2}, {part2, Acc3-Acc1}};
 count_chars([$\"|T], Acc1, Acc2, Acc3) -> count_chars(T, Acc1 + 1, Acc2, Acc3 + 3);
 count_chars([$\\, B | T], Acc1, Acc2, Acc3) when (B == $\") or (B == $\\) -> count_chars(T, Acc1 + 2, Acc2 + 1, Acc3 + 4);
-count_chars([$\\, $x, C, D | T], Acc1, Acc2, Acc3) ->
-    IsHex = is_hex_char(C) and is_hex_char(D),
-    if
-        IsHex -> count_chars(T, Acc1 + 4, Acc2 + 1, Acc3 + +5);
-        true -> count_chars([$x|[C|[D|T]]], Acc1 + 1, Acc2 +1, Acc3 + 1)
-    end;
+count_chars([$\\, $x, _C, _D | T], Acc1, Acc2, Acc3) -> count_chars(T, Acc1 + 4, Acc2 + 1, Acc3 + +5);
 count_chars([_H|T], Acc1, Acc2, Acc3) -> count_chars(T, Acc1 + 1, Acc2 + 1, Acc3 + 1).
 
 
