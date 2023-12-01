3642bbe535df47986518f6d47b2f515304bfdac0
Lieven <lieven@risolto.be>
2023-12-01 09:11:56 +0100

    day8 even shorter :-)

--- day8.erl
+++ day8.erl
@@ -8,7 +8,6 @@ process_input(Path) ->
     {ok, File} = file:read_file(Path),
     flatten([binary_to_list(X) || X <- split(File, <<"\n">>, [global])]).
 
-count(Chars) -> count(Chars, 0, 0, 0).
 count([], Acc1, Acc2, Acc3) -> {{part1, Acc1-Acc2}, {part2, Acc3-Acc1}};
 count([$\"|T], Acc1, Acc2, Acc3) -> count(T, Acc1 + 1, Acc2, Acc3 + 3);
 count([$\\, B | T], Acc1, Acc2, Acc3) when (B == $\") or (B == $\\) -> count(T, Acc1 + 2, Acc2 + 1, Acc3 + 4);
@@ -17,4 +16,4 @@ count([_H|T], Acc1, Acc2, Acc3) -> count(T, Acc1 + 1, Acc2 + 1, Acc3 + 1).
 
 solve() ->
     Input = process_input("./input/day8.txt"),
-    timed(fun() -> count(Input) end).
+    timed(fun() -> count(Input,0,0,0) end).
